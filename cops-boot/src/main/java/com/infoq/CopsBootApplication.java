faa89a47ce709f025534e8fd5a203e1eba02c011
Hugo Mozo <hmozo@outlook.com>
2021-01-21 22:31:50 +0100

    config

---
+++ cops-boot/src/main/java/com/infoq/CopsBootApplication.java
@@ -0,0 +1,13 @@
+package com.infoq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopsBootApplication.class, args);
+	}
+
+}
